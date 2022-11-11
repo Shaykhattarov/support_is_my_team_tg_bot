197315ed54ea6b9a8306cd7726449d6d99295922
Ildan <ildan201@yandex.ru>
2022-11-11 17:10:51 +0400

    Правки в скрипте получения текста из файла

--- texts.py
+++ texts.py
@@ -7,10 +7,18 @@ class Text:
     welcome_text: str
 
     def __init__(self):
-        self.welcome_text = self.read_json_file()["welcome_text"]["text"]
+        self.welcome_text = self.read_json_file(self.__filename)["answer1"]["text"]
 
     @staticmethod
     def read_json_file(filename='data.json') -> dict:
-        with open(os.path.join(__file__, filename), 'r') as file:
-            data = json.loads(file.read())
+        if filename in os.listdir(os.path.join(__file__)):
+            with open(os.path.join(__file__, filename), 'r') as file:
+                data = json.loads(file.read())
+        else:
+            data: dict = {}
+            data['answer1']['text'] = "Текст не найден!"
+            with open(os.path.join(__file__, filename), 'x') as file:
+                json.dumps(obj=data, fp=file)
         return data
+
+
