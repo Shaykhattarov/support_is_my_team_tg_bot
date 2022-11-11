af328768c7fe3b76b9f819b10b8164857d13cf94
Ildan <ildan201@yandex.ru>
2022-11-11 18:35:58 +0400

    Small changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import keyboards as kb
 
-from texts import Text
+from texts import welcome_text
 from aiogram import Bot, Dispatcher, executor, types
 
 
@@ -11,9 +11,8 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(command=['start'])
 async def information_text(message: types.Message):
-    await message.reply(text.welcome_text, reply_markup=kb.inline_link_to_questionnaire_kb)
+    await message.reply(welcome_text, reply_markup=kb.inline_link_to_questionnaire_kb)
 
 
 if __name__ == "__main__":
-    text = Text()
     executor.start_polling(dp)
--- texts.py
+++ texts.py
@@ -1,24 +1,27 @@
 import os
 import json
 
+with open('data.json', 'r') as file:
+    welcome_text = json.load(file)
 
-class Text:
-    __filename: str = 'data.json'
-    welcome_text: str = 'Привет'
-
-    def __init__(self):
-        self.welcome_text = self.read_json_file(self.__filename)["answer1"]["text"]
-
-    @staticmethod
-    def read_json_file(filename='data.json') -> dict:
-        if filename in os.listdir(os.path.join(__file__)):
-            with open(os.path.join(__file__, filename), 'r') as file:
-                data = json.loads(file.read())
-        else:
-            data: dict = {}
-            data['answer1']['text'] = "Текст не найден!"
-            with open(os.path.join(__file__, filename), 'x') as file:
-                json.dumps(obj=data, fp=file)
-        return data
-
+#
+# class Text:
+#     __filename: str = 'data.json'
+#     welcome_text: str = 'Привет'
+#
+#     def __init__(self):
+#         self.welcome_text = self.read_json_file(self.__filename)["answer1"]["text"]
+#
+#     @staticmethod
+#     def read_json_file(filename='data.json') -> dict:
+#         if filename in os.listdir(os.path.join(__file__)):
+#             with open(os.path.join(__file__, filename), 'r') as file:
+#                 data = json.loads(file.read())
+#         else:
+#             data: dict = {}
+#             data['answer1']['text'] = "Текст не найден!"
+#             with open(os.path.join(__file__, filename), 'x') as file:
+#                 json.dumps(obj=data, fp=file)
+#         return data
+#
 
