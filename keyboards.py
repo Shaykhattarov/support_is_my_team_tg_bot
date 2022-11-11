bfabe0d3f8883dd9f91317af6124af487bf2d4cd
Ildan <ildan201@yandex.ru>
2022-11-11 22:09:50 +0400

    Change button url

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-inline_link_to_questionnaire_btn = InlineKeyboardButton('Продолжить', url='https://vk.com/audio-2001775534_63775534')
+inline_link_to_questionnaire_btn = InlineKeyboardButton('Продолжить', url='https://docs.google.com/forms/d/e/1FAIpQLSfX8Cqpz4Q5rpJILvq7K0j2_2PT9HkevEpR0jJ7-um5TADcgw/viewform?usp=sharing')
 inline_link_to_questionnaire_kb = InlineKeyboardMarkup().add(inline_link_to_questionnaire_btn)
