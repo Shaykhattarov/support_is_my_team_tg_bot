05a1e402902fd7b130a99bb286172af6352f1ca0
Ildan <ildan201@yandex.ru>
2022-11-11 18:43:32 +0400

    Small changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import keyboards as kb
 
-from texts import welcome_text
+import texts
 from aiogram import Bot, Dispatcher, executor, types
 
 
@@ -11,8 +11,9 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(command=['start'])
 async def information_text(message: types.Message):
-    await message.reply(welcome_text, reply_markup=kb.inline_link_to_questionnaire_kb)
+    await message.reply(texts.get_text(filename), reply_markup=kb.inline_link_to_questionnaire_kb)
 
 
 if __name__ == "__main__":
+    filename: str = 'data.json'
     executor.start_polling(dp)
