f9d1b388c8863507d5f6db516210d542777e7f07
Ildan <ildan201@yandex.ru>
2022-11-11 18:30:35 +0400

    Small changes

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from texts import Text
 from aiogram import Bot, Dispatcher, executor, types
 
 
-bot = Bot(token=os.getenv("TG_BOT_TOKEN"))
+bot = Bot(token=os.getenv("OPORA_MY_TEAM_TOKEN"))
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(command=['start'])
 async def information_text(message: types.Message):
-    await message.reply(text.welcome_text)
+    await message.reply(text.welcome_text, reply_markup=kb.inline_link_to_questionnaire_kb)
 
 
 if __name__ == "__main__":
